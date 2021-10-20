523a114b3a6a5ee769886e10ab6d9bbb67033600
Grant <grantguenter@gmail.com>
2021-10-20 00:52:53 -0700

    Create linked models, detailedrecipe from recipe.

---
+++ recipe/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+import computation
+import unittest
+
+# Create your tests here.
+
+
+class TestComputation(unittest.TestCase):
+    def test_closest_str(self):
+        self.assertEqual()
